5dd98e1d6f204c911bd29ed91b5bea4fb261c0bb
leclere <leclere@localhost.local>
2025-10-19 01:50:11 +0200

    Ajout structure javadoc sur GestionBdd + MàJ Makefile

--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@ default : run
 
 run : compil exec
 
-
 compil: 
-	javac -d build -classpath build -sourcepath "src:tmp" src/fr/iutfbleau/remindme/*.java
+	javac -d build -classpath "build:/export/documents/mariadb-client.jar" -sourcepath "src:tmp" src/fr/iutfbleau/remindme/*.java
 
 exec:
-	java -classpath "build:/export/documents/mariadb-client.jar fr.iutfbleau.remindme.StartApp"
+	java -classpath "build:/export/documents/mariadb-client.jar" fr.iutfbleau.remindme.StartApp
--- src/fr/iutfbleau/remindme/GestionBdd.java
+++ src/fr/iutfbleau/remindme/GestionBdd.java
@@ -4,15 +4,23 @@
 import org.mariadb.jdbc.*;
 import java.sql.*;
 
-
+/**
+ * Classe qui établi la connexion à la base de données. 
+ * Les fonctions permettent d'intéragir avec les éléments 
+ * de la base de données
+ */
 
 
 public class GestionBdd{
 
+	/**
+	 * Connexion vers la base de données
+	 */
+	protected Connection cnx; 
 
-	Connection cnx; 
-
-
+	/**
+	 * Etabli la connexion avec la base de données cible et la stocke dans cnx
+	 */
 	public GestionBdd(){
 
 		try{
@@ -28,33 +36,55 @@ public class GestionBdd{
 		
 	}
 	/**
+	 * ajoute un tuple dans la base de données
 	 * 
+	 * @param titre le titre du nouvel tuple 
+	 * @param contenu le contenu du nouvel tuple
+	 * @param rang le rang du nouvel tuple
+	 * @param theme le theme du nouvel tuple
 	 **/
-	public void ajouterElem(String titre, String contenu, int rang, String couleur){
+	public void ajouterElem(String titre, String contenu, int rang, String theme){
 		try{
 			PreparedStatement pst = this.cnx.prepareStatement("INSERT INTO Rappel(titre,contenu,rang,theme) Values(?,?,?,?)");
 			pst.setString(1,titre);
 			pst.setString(2,contenu);
 			pst.setInt(3,rang);
-			pst.setString(4,couleur);
+			pst.setString(4,theme);
 			pst.executeUpdate();
 			pst.close();
 		}catch (SQLException e){
-			System.out.println("Erreur ajout element\n"+e.getMessage());
+			System.out.println("Erreur ajout tuple\n"+e.getMessage());
 		}
 	}
 
-	public void ajouterElem(String titre,int rang, String couleur){
-		ajouterElem(titre,null,rang,couleur);
+	/**
+	 * ajout un tuple dans la base de donnée en définissant le contenu à null
+	 * 
+	 * @param titre le titre du nouvel element 
+	 * @param rang le rang du nouvel element
+	 * @param theme le theme du nouvel element
+	 */
+	public void ajouterElem(String titre,int rang, String theme){
+		ajouterElem(titre,null,rang,theme);
 	}
 
-	public void maj(int id, String titre, String contenu, int rang,String couleur){
+
+	/**
+	 * Permet de mettre à jour un tuple dans la base de données
+	 * 
+	 * @param id l'id du tuple qui sera modifié
+	 * @param newTitre le nouveau titre du tuple
+	 * @param newContenu le nouveau contenu du tuple 
+	 * @param newRang le nouveau rang du tuple
+	 * @param newTheme le nouveau theme du tuple
+	 */
+	public void maj(int id, String newTitre, String newContenu, int newRang,String newTheme){
 		try{
 			PreparedStatement pst = this.cnx.prepareStatement("UPDATE Rappel SET titre = ?, contenu = ?, rang = ?, theme = ? WHERE id = ?");
-			pst.setString(1,titre);
-			pst.setString(2,contenu);
-			pst.setInt(3,rang);
-			pst.setString(4,couleur);
+			pst.setString(1,newTitre);
+			pst.setString(2,newContenu);
+			pst.setInt(3,newRang);
+			pst.setString(4,newTheme);
 			pst.setInt(5,id);
 			pst.executeUpdate();
 			pst.close();
@@ -90,6 +120,11 @@ public class GestionBdd{
 		}
 	}*/
 
+	/**
+	 * Supprime un tuple de la base de données à partir de son id
+	 * 
+	 * @param id l'id du tuple qui sera supprimé
+	 */
 	public void supprimer(int id){
 		try{
 			PreparedStatement pst = this.cnx.prepareStatement("DELETE FROM Rappel WHERE id = ?");
@@ -102,7 +137,11 @@ public class GestionBdd{
 	}
 
 
-
+	/**
+	 * Renvoi tous les tuples stocké dans la base de données
+	 * 
+	 * @return tous les tuples stocké dans la base de données
+	 */
 	public ResultSet getEvents(){
 		try{
 			PreparedStatement pst = this.cnx.prepareStatement("SELECT * FROM Rappel");
@@ -113,7 +152,9 @@ public class GestionBdd{
 		return null;
 	}
 
-
+	/**
+	 * Ferme la connexion avec la base de données
+	 */
 	public void fermeConn(){
 		try{
 			this.cnx.close();
